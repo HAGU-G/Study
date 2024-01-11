4725e828141e099421a86d02a86628130bb677ce
조병민 <jsjbm@naver.com>
2024-01-11 11:27:59 +0900

    11:27

--- 2024-01/2024-01-11/main.c
+++ 2024-01/2024-01-11/main.c
@@ -1,10 +1,15 @@
+//전역변수와 지역변수의 차이를 발견
+
 #include<stdio.h>
 
-int ab;
+int a; //전역변수
 
 int main()
 {
 	//자신의 이름을 출력
-	printf("%d 조병민\n", ab);
+	printf("%d 조병민\n", a); //초기화를 하지않아도 오류가 발생하지 않음.
+
+	int b; //지역변수
+	printf("%d 조병민\n", b); //초기화를 하지않아서 오류발생.
 	return 0;
 }
