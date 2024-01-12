10457066bb1494d67afa73a89df41fd21d0500c2
조병민 <jsjbm@naver.com>
2024-01-12 09:32:21 +0900

    parcticeProblem review

--- 2024-01/2024-01-11/practiceProblem.c
+++ 2024-01/2024-01-11/practiceProblem.c
@@ -6,7 +6,9 @@ int main()
 	double double1, double2;
 	printf("두 실수\v 입력: ");
 	scanf("%lf %lf", &double1, &double2);
-	printf("%0.2lf + %0.1lf = %0.2lf\n", double1, double2, double1 + double2);
+	printf("%0.2f + %0.1f = %0.2f\n", double1, double2, double1 + double2);
+	//printf에 double과 float이 전달되면 자동으로 double형으로 변환
+	//printf는 %lf나 %f 상관 없다.
 
 	printf("\n2.\n"); // 2
 	double weight;
@@ -15,11 +17,13 @@ int main()
 	scanf("%lf", &weight);
 	printf("키(m) 입력: ");
 	scanf("%lf", &height);
-	printf("BMI : %lf\n", (weight / (height * height)));
+	printf("BMI : %f\n", (weight / (height * height)));
 
 	printf("\n3.\n"); // 3
 	char alphabet;
-	scanf(" %c", &alphabet);
+	scanf(" %c", &alphabet); //scanf는 return값도 입력받는다.
 	printf("%c", alphabet + 1);
+
+	return 0;
 
 }
