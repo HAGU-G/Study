fb63ca44c43f6104f52c675b1e3f709291d629f9
조병민 <jsjbm@naver.com>
2024-01-17 03:10:19 +0900

    스테이지 추가/수정 오브젝트 기호 수정

--- 2024-01/2024-01-16/main.cpp
+++ 2024-01/2024-01-16/main.cpp
@@ -3,7 +3,27 @@
 
 int main()
 {
-	int a;
+
+		int arr[10];
+		int(*p_arr)[10] = &arr;
+		int a[3] = {1, 2, 30};
+		int b[3];
+		int* ptr = a;
+		int* ptrb = b;
+
+		printf("%d\n", sizeof(p_arr+1));
+		printf("%d\n", p_arr);
+		printf("%d\n", p_arr + 1);
+
+
+		int arr2[7][10];
+		p_arr = arr2; //문제없이 작동한다.
+
+		//배열 포인터 p_arr
+
+
+
+	/*int a;
 	double b[20];
 
 	printf("%p\n", &a);
@@ -11,7 +31,7 @@ int main()
 
 	int c = (int)&b - (int)&a;
 	printf("%d\n", c);
-	
+	*/
 	// 다른 자료형끼리 연속으로 선언하면 주소는 (자료형 크기 + 20바이트) 차이가 난다??
 	// 같은 자료형끼리 연속으로 선언하면 주소는 32바이트 차이가 난다.
 	// 
@@ -37,11 +57,11 @@ int main()
 
 
 
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 }; //int형의 주민이 살 수 있는 아파트의 i호들을 지었다.
-	printf("%d\n", arr[2]); //주민.
-	printf("%p\n", &arr[2]); //주민이 살고 있는 호 ex) 101호
-	printf("%p\n", arr); //각 호들을 포함하는 아파트 ex) 코딩아파트
-	printf("%p\n", &arr); //아파트의 주소 ex) 플밍동 어렵로 24-441
+	//int arr[10] = { 1,2,3,4,5,6,7,8,9,10 }; //int형의 주민이 살 수 있는 아파트의 i호들을 지었다.
+	//printf("%d\n", arr[2]); //주민.
+	//printf("%p\n", &arr[2]); //주민이 살고 있는 호 ex) 101호
+	//printf("%p\n", arr); //각 호들을 포함하는 아파트 ex) 코딩아파트
+	//printf("%p\n", &arr); //아파트의 주소 ex) 플밍동 어렵로 24-441
 	// 
 
 
--- Practice/Board/main.cpp
+++ Practice/Board/main.cpp
@@ -3,38 +3,37 @@
 #include <conio.h>
 
 #define MAXROW 10 //최대 행 *수정 가능
-#define MAXCOL 13 //최대 열 *수정 가능
-#define ACTCHAR 100 //이전 행동 출력 최대 크기 *수정 가능
+#define MAXCOL 14 //최대 열 *수정 가능
+#define ACTCHAR 200 //이전 행동 출력 최대 크기 *수정 가능
 #define STAGEOBJECT MAXROW * MAXCOL * 2 //오브젝트 정보의 최대 크기
 
 char stage[MAXROW][MAXCOL][2]; //실시간 스테이지 오브젝트 정보를 담을 배열
-
 int selectStageNum; //선택한 스테이지 번호
 int selectStageRow; //선택한 스테이지 행
 int selectStageCol; //선택한 스테이지 열
 
-int playerRow; //플레이어 행
-int playerCol; //플레이어 열
-char active[ACTCHAR] = ""; //행동(입력)
+int playerRow; //플레이어 위치 : 행
+int playerCol; //플레이어 위치 : 열
+int moveCount = 0; //움직인 횟수
+char active[ACTCHAR] = ""; //이전 행동 기록
 
-
-//타일 종류 (추가 시 printTile, isMovable 작성)
+//타일 종류 (추가 시 printTile, isMove, isPush 작성)
 char tile_player = 'P';	//플레이어
 char tile_air = '0';	//빈칸
 char tile_ground = 'G';	//땅
 char tile_clear = '1';	//클리어
-char tile_ball = 'B';
+char tile_ball = 'B';	//공
 
 void printTile(char tile_name)
 {
 	if (tile_player == tile_name)
-		printf("＆");
+		printf("♀");
 	else if (tile_air == tile_name)
 		printf("　");
 	else if (tile_ground == tile_name)
 		printf("▩");
 	else if (tile_clear == tile_name)
-		printf("★");
+		printf("♨");
 	else if (tile_ball == tile_name)
 		printf("㉿");
 };
@@ -62,34 +61,31 @@ bool isPush(char tile_name)
 	}
 }
 
-//화면에 그리기
+//게임화면
 void draw()
 {
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0, 0 });
-	for (int i = 0; i < MAXCOL + 2; ++i) { printTile(tile_ground); } printf("\n");
 
+	for (int i = 0; i < MAXCOL + 2; ++i) { printTile(tile_ground); } printf("\n");
 	for (int i = 0; i < (MAXROW - selectStageRow) / 2; ++i) { for (int i = 0; i < MAXCOL + 2; ++i) { printTile(tile_ground); } printf("\n"); }
+	
 	for (int i = 0; i < selectStageRow; i++)
 	{
 		for (int i = 0; i < (MAXCOL - selectStageCol) / 2 + 1; ++i) { printTile(tile_ground); }
-		for (int j = 0; j < selectStageCol; j++)
-		{
-			printTile(stage[i][j][0]);
-		}
+		for (int j = 0; j < selectStageCol; j++) { printTile(stage[i][j][0]); }
 		for (int i = 0; i < (MAXCOL - selectStageCol) / 2 + 1; ++i) { printTile(tile_ground); } if (MAXCOL % 2 != selectStageCol % 2) { printTile(tile_ground); } printf("\n");
 	}
 
 	for (int i = 0; i < (MAXROW - selectStageRow) / 2; ++i) { for (int i = 0; i < MAXCOL + 2; ++i) { printTile(tile_ground); } printf("\n"); }
 	if (MAXROW % 2 != selectStageRow % 2) { for (int i = 0; i < MAXCOL + 2; ++i) { printTile(tile_ground); } printf("\n"); }
+	for (int i = 0; i < MAXCOL + 2; ++i) { printTile(tile_ground); } printf("\n");
 
-
-	for (int i = 0; i < MAXCOL + 2; ++i) { printTile(tile_ground); } printf("\n\n");
-
-	printf("　　　　↑　　　재시작：Enter\n이동：←↓→　　종료：ESC\n\n");
-	printf("이전 행동 : %s", active);
+	printf("\n　　　　↑　　　재시작：Enter\n이동：←↓→　　종료　：ESC\n\n");
+	printf("%s　　\n\n", active);
+	printf("< Stage %d > -- %d턴 째　", selectStageNum, moveCount);
 }
 
-//스테이지에 넣어야할 멤버
+//스테이지에 넣어야할 정보
 typedef struct {
 	const int num;
 	const int row;
@@ -98,43 +94,62 @@ typedef struct {
 } NEWSTAGE;
 
 //스테이지 목록
-NEWSTAGE Stage[5] = {
+NEWSTAGE Stage[] = {
 	{ 1, 6, 6,
 	"0 0 0 0 0 0 "
 	"0 0 0 0 0 0 "
-	"0 0 0 1 0 0 "
+	"0 0 0 0 0 1 "
 	"0 0 0 0 0 0 "
 	"0 0 0 0 0 0 "
 	"0 P 0 0 0 0"}
 	, { 2, 4, 9,
 	"1 0 0 0 G 0 0 0 0 "
-	"0 0 G 0 G 0 0 P 0 "
+	"0 0 G 0 G 0 P 0 0 "
 	"0 0 G 0 G 0 0 0 0 "
 	"0 0 G 0 0 0 0 0 0"}
-	,{ 3, 7, 3,
-	"P G 1 "
-	"0 G 0 "
-	"0 G 0 "
-	"0 G 0 "
-	"B 0 0 "
-	"0 G 0 "
-	"0 0 0"}
-	,{ 4, 10, 10,
-	"0 0 0 0 G 0 0 0 0 P "
-	"0 0 0 B 0 0 0 0 0 0 "
-	"0 G G G G G G B B G "
-	"0 G 0 1 G 0 0 0 0 G "
-	"0 G 0 G G 0 0 0 0 G "
-	"0 0 B 0 G B G 0 0 G "
-	"0 G 0 G 0 0 G 0 0 G "
-	"0 0 0 0 0 0 G G 0 0 "
-	"G G G 0 G B 0 0 0 0 "
-	"G G G 0 0 0 G G 0 0"}
-	,{ 5, 4, 4,
-	"0 0 0 0 "
+	,{ 3, 7, 9,
+	"G G 0 0 0 0 G G G "
+	"G G 0 B G 1 G G G "
+	"P G 0 0 G 0 G G G "
+	"0 G G B B 0 0 0 0 "
+	"0 G 0 0 G G G G G "
+	"0 B 0 0 G G G G G "
+	"G G 0 0 G G G G G"}
+	,{ 4, 5, 7,
+	"G 0 0 0 P G G "
+	"G 0 0 B B 1 G "
+	"G 0 0 0 B 0 G "
+	"G 0 0 0 0 G G "
+	"G G G 0 0 0 0 "
+	"G G G G 0 G G"}
+	,{ 5, 5, 4,
+	"0 B 0 G "
 	"0 0 B P "
-	"0 0 0 0 "
-	"0 0 0 0"}
+	"1 B B B "
+	"0 B 0 0 "
+	"G 0 0 G"}
+	,{ 6, 10, 14,
+	"G G 0 0 0 1 G 0 0 0 0 0 0 0 "
+	"G 0 B B G G G B G 0 0 0 0 0 "
+	"0 0 B 0 0 0 B 0 B 0 0 0 0 0 "
+	"G 0 0 0 B B G 0 G 0 0 0 0 0 "
+	"0 0 0 0 G P B 0 B 0 0 0 0 0 "
+	"0 0 0 0 B 0 G 0 B 0 0 0 0 0 "
+	"0 0 0 0 B 0 0 0 G 0 0 0 0 0 "
+	"0 0 0 0 0 G B B 0 0 0 0 0 0 "
+	"0 0 0 0 0 0 0 0 0 0 0 0 0 0 "
+	"0 0 0 0 0 0 0 0 0 0 0 0 0 0"}
+	,{ 7, 10, 14,
+	"0 0 0 0 0 P 0 0 0 0 0 0 0 0 "
+	"0 0 0 0 0 0 0 B 0 0 0 0 0 0 "
+	"0 B 0 0 0 0 B 0 0 0 0 B 0 0 "
+	"0 0 0 0 B 0 0 0 0 0 0 0 0 0 "
+	"0 0 0 0 0 0 0 0 B 0 0 0 0 0 "
+	"0 0 0 0 0 0 0 0 B 0 0 0 0 0 "
+	"0 0 0 0 B 0 0 0 0 0 0 B 0 0 "
+	"0 0 B 0 0 0 B 0 0 0 0 0 0 0 "
+	"0 0 0 B 0 0 0 0 0 0 0 0 0 0 "
+	"0 0 0 0 0 0 0 0 0 0 0 0 0 0"}
 
 
 };
@@ -142,6 +157,7 @@ NEWSTAGE Stage[5] = {
 //스테이지 선택 및 불러오기
 void selectStage(int stageNum)
 {
+	moveCount = 1;
 	char object[STAGEOBJECT] = "";
 	char* buffer = nullptr;
 
@@ -207,6 +223,7 @@ void act(int input)
 				stage[playerRow][playerCol][0] = tile_air;
 				playerCol--;
 				strcpy_s(active, ACTCHAR, "왼쪽으로 이동 　　");
+				moveCount++;
 			}
 			else if (isPush(stage[playerRow][playerCol - 1][0]))
 			{
@@ -217,6 +234,7 @@ void act(int input)
 					stage[playerRow][playerCol][0] = tile_air;
 					playerCol -= 1;
 					strcpy_s(active, ACTCHAR, "공을 왼쪽으로 이동");
+					moveCount++;
 				}
 			}
 		}
@@ -236,6 +254,7 @@ void act(int input)
 				stage[playerRow][playerCol][0] = tile_air;
 				playerRow++;
 				strcpy_s(active, ACTCHAR, "아래로 이동　　 　");
+				moveCount++;
 			}
 			else if (isPush(stage[playerRow + 1][playerCol][0]))
 			{
@@ -246,6 +265,7 @@ void act(int input)
 					stage[playerRow][playerCol][0] = tile_air;
 					playerRow += 1;
 					strcpy_s(active, ACTCHAR, "공을 아래로 이동　");
+					moveCount++;
 				}
 			}
 		}
@@ -265,6 +285,7 @@ void act(int input)
 				stage[playerRow][playerCol][0] = tile_air;
 				playerCol++;
 				strcpy_s(active, ACTCHAR, "오른쪽으로 이동 　");
+				moveCount++;
 			}
 			else if (isPush(stage[playerRow][playerCol + 1][0]))
 			{
@@ -275,6 +296,7 @@ void act(int input)
 					stage[playerRow][playerCol][0] = tile_air;
 					playerCol += 1;
 					strcpy_s(active, ACTCHAR, "공을 우측으로 이동");
+					moveCount++;
 				}
 			}
 		}
@@ -294,6 +316,7 @@ void act(int input)
 				stage[playerRow][playerCol][0] = tile_air;
 				playerRow--;
 				strcpy_s(active, ACTCHAR, "위로 이동　　　 　");
+				moveCount++;
 			}
 			else if (isPush(stage[playerRow - 1][playerCol][0]))
 			{
@@ -304,6 +327,7 @@ void act(int input)
 					stage[playerRow][playerCol][0] = tile_air;
 					playerRow -= 1;
 					strcpy_s(active, ACTCHAR, "공을 위로 이동　　");
+					moveCount++;
 				}
 			}
 		}
@@ -321,13 +345,13 @@ int main()
 {
 	//콘솔 설정
 	{
-		system("mode con:cols=30 lines=18");
+		system("mode con:cols=34 lines=20");
 		CONSOLE_CURSOR_INFO cursorVisivle = { 100, FALSE };
 		SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorVisivle);
 	}
 
 	int input;
-	selectStage(1);
+	selectStage(6);
 
 	do
 	{
