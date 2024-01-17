e97577612db3335fa243d5290bb4b34c02a98a31
조병민 <jsjbm@naver.com>
2024-01-17 13:48:27 +0900

    2024-01-17 problem added

--- 2024-01/2024-01-17/funcPractice02.cpp
+++ 2024-01/2024-01-17/funcPractice02.cpp
@@ -1,11 +1,24 @@
 #include <stdio.h>
 
-void Swap(int a, int b) //함수의 이름은 동사형태로 지정, 변수는 명사형태
+void SwapValue(int a, int b) //함수의 이름은 동사형태로 지정, 변수는 명사형태
 {
 	int temp = a;
 	a = b;
 	b = temp;
+}
 
+void SwapAddress(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void SwapReference(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
 }
 
 
@@ -14,9 +27,25 @@ int main()
 	int x = 10;
 	int y = 20;
 
-	Swap(x, y);
+	SwapValue(x, y);
+	printf("Value %d %d\n", x, y);
 
-	printf("%d %d", x, y);
+	SwapAddress(&x, &y);
+	printf("Address %d %d\n", x, y);
+
+	SwapReference(x, y);
+	printf("Reference %d %d\n", x, y);
+
+	int a = 10;
+	int& ref = a;
+	//ref는 a와 똑같은 역할을 한다.
+	//선언과 동시에 초기화를 해야한다.
+
+	printf("%d %d\n", &a, &ref); //10 10
+	ref++;
+
+	printf("%d %d\n", a, ref); //11 11
+
 
 	return 0;
 }
---
+++ 2024-01/2024-01-17/problem.cpp
@@ -0,0 +1,20 @@
+/*
+int StrLen(char* str)
+{
+	return 0;
+}
+
+char str[] = "ABCDEF"
+int len = StrLen(str);
+*/
+#include <stdio.h>
+
+int main()
+{
+	{
+
+	}
+
+
+	return 0;
+}
